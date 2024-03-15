898ddacb4fb0fe5cd6abcf4da2da14e013d49181
youcastle03 <107834288+youcastle03@users.noreply.github.com>
2024-03-15 17:30:22 +0900

    [level 1] Title: 체육복, Time: 0.35 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,33 @@
+import java.util.*; 
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        Arrays.sort(reserve);
+        Arrays.sort(lost);
+        answer = n - lost.length;
+        //여벌도난당한사람
+        for (int i = 0; i < lost.length; i++) {
+			for (int j = 0; j < reserve.length; j++) {
+				if (lost[i] == reserve[j]) {
+					answer++;
+					lost[i] = -1;
+					reserve[j] = -1;
+                    break;
+				}
+			}
+		}
+        //도난당한 학생에게 체육복 빌려주기
+        for (int i = 0; i < lost.length; i++) {
+			for (int j = 0; j < reserve.length; j++) {
+				if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+					answer++;
+					reserve[j] = -1;
+					break;
+				}
+			}
+		}
+
+        
+        return answer;
+    }
+}
