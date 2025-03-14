db1616680cd10aed999f90e4669e9ee30fda7231
youcastle03 <107834288+youcastle03@users.noreply.github.com>
2025-03-15 00:31:18 +0900

    [level 1] Title: 예산, Time: 1.84 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
-       int answer = 0;
+        int answer = 0;
         Arrays.sort(d);
         for(int i=0; i<d.length; i++){
             if(d[i]<=budget){
