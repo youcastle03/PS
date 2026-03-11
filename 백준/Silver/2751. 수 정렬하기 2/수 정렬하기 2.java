985845bdb2efb289314d41c166860c918ddab078
youcastle03 <107834288+youcastle03@users.noreply.github.com>
2026-03-11 11:24:06 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2708 ms, Memory: 334252 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+        
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i=0; i<N ;i++){
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        for(int value : list){
+            sb.append(value).append('\n');
+        }
+        System.out.print(sb);
+
+	}
+}	
