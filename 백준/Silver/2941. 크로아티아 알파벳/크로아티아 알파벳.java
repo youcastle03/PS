d061621f0244be0f8ac8fbd78ddde2118bc3a768
youcastle03 <107834288+youcastle03@users.noreply.github.com>
2026-03-25 17:24:42 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 196 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+
+        st=st.replaceAll("c=","1");
+        st=st.replaceAll("c-","1");
+        st=st.replaceAll("dz=","1");
+        st=st.replaceAll("d-","1");
+        st=st.replaceAll("lj","1");
+        st=st.replaceAll("nj","1");
+        st=st.replaceAll("s=","1");
+        st=st.replaceAll("z=","1");
+        System.out.println(st.length());
+    }
+}
